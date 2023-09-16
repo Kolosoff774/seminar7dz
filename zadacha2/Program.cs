bc3591b0897369e9a2594fbe402a00757304f020
Andrey <akolosov48@gmail.com>
2023-09-16 11:01:11 +0500

    добавлены программы

---
+++ zadacha2/Program.cs
@@ -0,0 +1,75 @@
+﻿﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1, 7 -> такого элемента в массиве нет
+*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) 
+{
+    //                       0        1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+        Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+void PrintResult(int numR, int numC, int[,] matrix) 
+{
+    Console.Write($"{numR}, {numC} -> ");
+    if (SearchElementInMatrix(numR, numC, matrix) != 0) 
+    {
+        Console.WriteLine(SearchElementInMatrix(numR, numC, matrix));
+    } 
+    else 
+    {
+            Console.WriteLine("Такого элемента в массиве нет!");
+    }
+}
+
+int SearchElementInMatrix(int numR, int numC, int [,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            if (numR == i && numC == j) return matrix[i, j];
+        }
+    }
+    return 0;
+}
+
+Console.Write("Введи индекс строки: ");
+int numberRows = int.Parse(Console.ReadLine());
+Console.Write("Введи индекс столбца: ");
+int numberColumns = int.Parse(Console.ReadLine());
+
+int[,] array2d = CreateMatrixRndInt(5, 10, -100, 100);
+
+if (numberRows < 0 || numberColumns < 0) Console.WriteLine("Вы ввели отрицательный индекс!");
+else {
+PrintMatrix(array2d);
+PrintResult(numberRows, numberColumns, array2d);
+}
---
+++ zadacha3/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) 
+{
+    //                       0        1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+        Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[] AverageElemDiagonalMatrix(int[,] matrix) 
+{
+    int sum = 0;
+    double x = matrix.GetLength(0);
+    double[] res = new double[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++) 
+        {
+            sum += matrix[j, i];
+        }
+        res[i] = sum / x;
+        sum = 0;
+    }
+    return res;
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 5, 1, 9);
+PrintMatrix(array2d);
+
+double[] averElem = AverageElemDiagonalMatrix(array2d);
+Console.Write("Среднее арифметическое каждого столбца: ");
+for (int i = 0; i < averElem.Length; i++) 
+{
+    if (i < averElem.Length - 1) Console.Write($"{Math.Round(averElem[i], 1)}, ");
+    else Console.Write(averElem[i]);
+}
